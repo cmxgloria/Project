fe4463ba17617891b1247b1232028b80bbf49c52
Gloria Chen <cmxgloria@gmail.com>
2020-01-05 10:55:56 +1100

    updated

--- PT-Planner/ptplanner.js
+++ PT-Planner/ptplanner.js
@@ -28,62 +28,115 @@ var trainNetwork = {
   sandringham: sandringham
 };
 
-var sliceStops = function(originIndex, destinationInde)
+var sliceStops = function(originIndex, destinationIndex, arr) {
+  var stops = [];
+  if (originIndex < destinationIndex) {
+    stops = arr.slice(originIndex, destinationIndex + 1);
+  } else {
+    stops = arr.slice(destinationIndex, originIndex + 1).reverse();
+  }
+  return stops;
+};
 
+var printStops = function(firstLineStops, secondLineStops) {
+  if (secondLineStops) {
+    console.log(`${firstLineStops.join(" ----> ")}
+                                                    || ${secondLineStops.join(
+                                                      " ----> "
+                                                    )}`);
+  } else {
+    console.log(firstLineStops.join(" ----> "));
+  }
+};
 var trainTravel = function(origin, destination) {
   if (origin === destination) {
     console.error("Origin and destination can not be the same");
     return;
   }
-  var originalObj;
+  var originObj;
   var destinationObj;
   for (const line in trainNetwork) {
     var findOriginIndex = trainNetwork[line].indexOf(origin);
     var findDestinationIndex = trainNetwork[line].indexOf(destination);
     if (findOriginIndex !== -1) {
-      originalObj = { index: findOriginIndex, line: line };
+      originObj = { index: findOriginIndex, line: line };
     }
     if (findDestinationIndex !== -1) {
       destinationObj = { index: findDestinationIndex, line: line };
     }
   }
-  if (!originalObj || !destinationObj) {
+  if (!originObj || !destinationObj) {
     console.error("Make sure you type the correct origin and destination");
     return;
   }
   if (origin === "Richmond") {
     var richmondIndex = trainNetwork[destinationObj.line].indexOf("Richmond");
-    var stops = [];
-    if (richmondIndex < destinationObj.index) {
-      stops = trainNetwork[destinationObj.line].slice(
-        richmondIndex,
-        destinationObj.index + 1
-      );
-    } else {
-      stops = trainNetwork[destinationObj.line].slice(
-        destinationObj.index,
-        richmondIndex + 1
-      ).reverse();
-    }
-    console.log(stops.join(" ----> "));
+    var stops = sliceStops(
+      richmondIndex,
+      destinationObj.index,
+      trainNetwork[destinationObj.line]
+    );
+    // var stops = [];
+    // if (richmondIndex < destinationObj.index) {
+    //   stops = trainNetwork[destinationObj.line].slice(
+    //     richmondIndex,
+    //     destinationObj.index + 1
+    //   );
+    // } else {
+    //   stops = trainNetwork[destinationObj.line]
+    //     .slice(destinationObj.index, richmondIndex + 1)
+    //     .reverse();
+    // }
+    printStops(stops);
   } else if (destination === "Richmond") {
-    var richmondIndex = trainNetwork[originalObj.line].indexOf("Richmond");
-    var stops = [];
-    if (richmondIndex < originalObj.index) {
-      stops = trainNetwork[originalObj.line].slice(
-        richmondIndex,
-        originalObj.index + 1
-      );
-    } else {
-      stops = trainNetwork[originalObj.line].slice(
-        originalObj.index,
-        richmondIndex + 1
-      ).reverse();
-    }
-    console.log(stops.join(" ----> "));
-  } else if (originalObj.line === destinationObj.line) {
-
+    var richmondIndex = trainNetwork[originObj.line].indexOf("Richmond");
+    var stops = sliceStops(
+      originObj.index,
+      richmondIndex,
+      trainNetwork[originObj.line]
+    );
+    // var stops = [];
+    // if (richmondIndex < originObj.index) {
+    //   stops = trainNetwork[originObj.line].slice(
+    //     richmondIndex,
+    //     originObj.index + 1
+    //   );
+    // } else {
+    //   stops = trainNetwork[originObj.line]
+    //     .slice(originObj.index, richmondIndex + 1)
+    //     .reverse();
+    // }
+    printStops(stops);
+  } else if (originObj.line === destinationObj.line) {
+    var stops = sliceStops(
+      originObj.index,
+      destinationObj.index,
+      trainNetwork[originObj.line]
+    );
+    printStops(stops);
+  } else {
+    // different lines
+    var richmondInOriginIndex = trainNetwork[originObj.line].indexOf(
+      "Richmond"
+    );
+    var originStops = sliceStops(
+      originObj.index,
+      richmondInOriginIndex,
+      trainNetwork[originObj.line]
+    );
+    var richmondInDestinationIndex = trainNetwork[destinationObj.line].indexOf(
+      "Richmond"
+    );
+    var destinationStops = sliceStops(
+      richmondInDestinationIndex,
+      destinationObj.index,
+      trainNetwork[destinationObj.line]
+    );
+    printStops(originStops, destinationStops);
   }
 };
 trainTravel("Richmond", "Tooronga");
 trainTravel("Tooronga", "Richmond");
+trainTravel("Glenferrie", "Windsor");
+trainTravel("Glenferrie", "Glenferrie");
+trainTravel("Flinder Street", "Heidelberg");
